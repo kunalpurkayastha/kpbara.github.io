bcbf0b5407aad7469b675155568cc6e6666d6e0d
kunalpurkayastha <kunalpurkayastha09@gmail.com>
2023-01-21 00:56:12 +0530

    portfolio#1

---
+++ app.js
@@ -0,0 +1,56 @@
+// // Form submission event listener
+// document.getElementById("contact-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     // Get the message from the input field
+//     var message = document.getElementById("message").value;
+//     // Send the message to the Discord bot
+//     sendMessageToDiscordBot(message);
+// });
+
+// // Function to send the message to the Discord bot
+// function sendMessageToDiscordBot(message) {
+//     // Your Discord bot's token
+//     var botToken = "MTA2NjA1NjMxNTc3ODEyNTkyNA.GdYUbB.6eG3tYxiXx-6WFzDo4-rR3x7aJOzCdjI8GopA0";
+//     // The ID of the Discord channel you want to send the message to
+//     var channelId = "1066054133368500265/1066054133368500268";
+//     // The message payload
+//     var payload = {
+//         "content": message
+//     };
+//     // Send the message to the Discord bot
+//     fetch("https://discordapp.com/api/v6/channels/" + channelId + "/messages", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Authorization": "Bot " + botToken
+//         },
+//         body: JSON.stringify(payload)
+//     })
+//     .then(function(response) {
+//         if (response.ok) {
+//             // Message sent successfully
+//             alert("Message sent!");
+//         } else {
+//             // An error occurred
+//             alert("Error: " + response.statusText);
+//         }
+//     })
+//     .catch(function(error) {
+//         // An error occurred
+//         alert("Error: " + error);
+//     });
+// }
+
+const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
